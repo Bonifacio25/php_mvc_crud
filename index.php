56a95ec707c49bc8b6feb3448bfa376438c211ff
Felipe Bonifacio <ticwayfelipe@gmail.com>
2023-07-06 20:00:01 -0300

    Estrutura html

---
+++ index.php
@@ -0,0 +1,123 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <style type="text/css">
+            *{
+                padding: 0px;
+                margin: 0px;
+            }
+            body{
+                background-color: #eeeeee;
+            }
+
+            #corpo{
+                background-color: #f6f6f6;
+                border-left: 2px solid #c32e58;
+                border-right: 2px solid #c32e58;
+                height: 100vh;
+                margin: 0 auto;
+                width: 1000px;
+            }
+
+            #topo{
+                padding: 20px 25px;
+                border-bottom: 2px solid #c32e58;
+            }
+
+            #topo a{
+                color: #161d34;
+                font: 24px Arial;
+                text-decoration: none;
+            }
+
+            #topo a:hover{
+                text-decoration: underline;
+            }
+
+            #conteudo{
+                background-color: #f6f6f6;
+                padding: 20px 25px;
+            }
+
+            #conteudo h1{
+                color: #161d34;
+                font: 22px Arial;
+                margin-bottom: 20px;
+            }
+
+            #conteudo article{
+                margin-bottom: 20px;
+            }
+
+            article h2{
+                margin-bottom: 7px;
+            }
+
+            article h2 a{
+                color: #161d34;
+                font: 22px Arial;
+                text-decoration: none;
+            }
+
+            article h2 a:hover{
+                text-decoration: underline;
+            }
+
+            article p{
+                color: #161d34;
+                padding-left: 2px;
+                font: 16px Arial;
+            }
+        </style>
+
+        <div id="corpo">
+            <div id="topo">
+                <header>
+                    <nav>
+                        <a href="#">Home</a> |
+                        <a href="#">Sobre</a>
+                    </nav>
+                 </header>
+            </div>
+
+            <div id="conteudo">
+                <h1>Postagens</h1>
+                <section>
+                    <article>
+                        <header>
+                            <h2><a href="#">Título da minha postagem</a></h2>
+                        </header>
+
+                        <p>Conteudo da minha postagem</p>
+                    </article>
+
+                    <article>
+                        <header>
+                            <h2><a href="#">Título da minha postagem</a></h2>
+                        </header>
+
+                        <p>Conteudo da minha postagem</p>
+                    </article>
+
+                    <article>
+                        <header>
+                            <h2><a href="#">Título da minha postagem</a></h2>
+                        </header>
+
+                        <p>Conteudo da minha postagem</p>
+                    </article>
+
+                    <article>
+                        <header>
+                            <h2><a href="#">Título da minha postagem</a></h2>
+                        </header>
+
+                        <p>Conteudo da minha postagem</p>
+                    </article>
+                </section>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+Palheta de cores:
+#132c53
+#d64953
+#f9f0e6
+
+ou
+
+#371209
+#f4f4f4
+#d8c6b3
+
+ou
+
+#6c3c31
+#b19e8c
+#efe5db
+
+este
+Cor do texto: #161d34
+Cor das bordas: #c32e58
+cor de fundo: #eeeeee
+cor body: #f6f6f6
+
+ou
+
+
+#c92d59
+#ffaa00
+#eeeeee
+
+ou
+
+#181818
+#ffaa00
+#eeeeee
